d81fc7a1204f9aab87b2aadac39adf741c567db1
Nitish Sundarraj <sundarrajnithish@gmail.com>
2024-10-01 17:22:02 -0400

    Update main.py

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -11,6 +11,12 @@ def hello_world():
     """Example Hello World route."""
 
     return f"Hello World!!!!!!"
+
+@app.route("/user", methods=['GET', 'POST'])
+def user_resource():
+    """Example Hello World route."""
+
+    return json.dumps({"id": 123, "name" :"jack"})
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
